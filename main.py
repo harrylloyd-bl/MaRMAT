5965ea535541d118f0ecf0bf71d9c629d5d0f891
harrylloyd-bl <harry.lloyd@bl.uk>
2025-02-18 17:18:02 +0000

    Formatting improvements

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from marmat.audit import AuditTool
 
 LEXICON_PATH = "data\\external\\bl_lexicon_plural.csv"
 INTERIM_PATH = "data\\interim\\"
-PROCESSED_PATH = "data\\processed\\restructure_test\\"
+PROCESSED_PATH = "data\\processed\\handle_duplicates\\"
 
 ALEPH = True
 IAMS = False
@@ -22,45 +22,43 @@ if __name__ == "__main__":
         ]
     }
 
-    print("Initialize the tool:")
+    print("Initialize")
     tool = AuditTool()
 
-    print("\nLoad lexicon and metadata files")
+    print("Loading lexicon and metadata files")
     tool.load_lexicon(LEXICON_PATH)  # Input the path to your lexicon CSV file.
 
     # Aleph
     if ALEPH:
-        print(f"\nSetting metadata and ID columns, and lexicon categories")
+        print(f"Setting metadata, ID columns, and lexicon categories")
         tool.select_columns(["Title [245]"])  # Input the name(s) of the metadata column(s) you want to analyze.
-        tool.select_identifier_column("System No [001]")
         tool.select_categories(["Race", "Enslavement"])
         tool.select_export_cols(["Personal Name [100]", "Corp Name [110]", "Date Pub [260 and 264$c]", "Shelfmark [852]"])
 
-        for f in record_files["aleph"][:-2]:
+        for f in record_files["aleph"][:1]:
             print(f"\nLoading {f}")
             if "Maps" in f or "Music" in f:
                 tool.select_export_cols(["Personal Name [100]", "Corp Name [110]", "Date Pub [264$c]", "Shelfmark [852]"])
-            tool.load_metadata(os.path.join(INTERIM_PATH, f"{f}.csv"))  # Input the path to your metadata CSV file
+            tool.load_metadata(os.path.join(INTERIM_PATH, f"{f}.csv"), id_col="System No [001]")  # Input the path to your metadata CSV file
 
-            print("\nPerform matching and exporting results")
+            print("Matching and exporting results")
             output_file = os.path.join(PROCESSED_PATH, f"{f}_matches.csv")  # Input the file path where you want to save your matches here.
             tool.perform_matching()
             tool.export_matches(output_file)
 
     #IAMS
     if IAMS:
-        print(f"\nSetting metadata and ID columns, and lexicon categories")
+        print(f"Setting metadata, ID columns, and lexicon categories")
         tool.select_columns(["Title", "Scope and content"])  # Input the name(s) of the metadata column(s) you want to analyze.
-        tool.select_identifier_column("Reference")
         tool.select_categories(["Race", "Enslavement", "Aggrandizement"])
-        tool.select_export_cols(["Date range"])
+        tool.select_export_cols(["Reference", "Date range"])
 
         for f in record_files["iams"]:
 
             print(f"\nLoading {f}")
-            tool.load_metadata(os.path.join(INTERIM_PATH, f"{f}.csv"))  # Input the path to your metadata CSV file
+            tool.load_metadata(os.path.join(INTERIM_PATH, f"{f}.csv"), id_col="Record ID")  # Input the path to your metadata CSV file
 
-            print("\nPerform matching and exporting results")
+            print("Matching and exporting results")
             output_file = os.path.join(PROCESSED_PATH, f"{f}_matches.csv")  # Input the file path where you want to save your matches here.
             tool.perform_matching()
             tool.export_matches(output_file)
